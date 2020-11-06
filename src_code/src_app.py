7c815bb2db20f3f055267ae4c415a724e9cae25a
emadarmiti <emadarmiti@gmail.com>
2020-11-06 17:37:35 +0200

    edit the port

--- src_code/src_app.py
+++ src_code/src_app.py
@@ -136,4 +136,4 @@ if __name__ == '__main__':
             pickle_file_path)
 
         # run the service, set the port
-        app.run(host="0.0.0.0", port=8000)
+        app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)))
